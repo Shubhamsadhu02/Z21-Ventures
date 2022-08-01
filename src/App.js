b2eba1abb891f7b1578e11a8f28a024f489934cb
shubham3000 <shubhamsadhu02@gmail.com>
2022-08-01 17:27:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import './App.css';
+import './Styles/global.css'
+import Home from './Components/Home';
+import Investment from "./Components/Investment";
+import OurTeam from "./Components/OurTeam";
+import ContactUs from "./Components/ContactUs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/investment" element={<Investment />} ></Route>
+        <Route path="/ourteam" element={<OurTeam />} ></Route>
+        <Route path="/contactus" element={<ContactUs />} ></Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/Components/InvestmentCompanies.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function InestmentCompanies() {
+  return (
+    <>
+    <section className='mt-15' style={{background: "#F9FAFA", padding: "150px 153px"}}>
+        <div className="container">
+            <div className="investment-header text-center">
+                <h2>Investments History</h2>
+            </div>
+            <div className="investment-companies">
+                {/* <ul>
+                    <li>
+                        <img src='/images/Investment/scaler.png' alt=''/>
+                    </li>
+                    <li>
+                        <img src='/images/Investment/mintoak.png' alt=''/>
+                    </li>
+                    <li>
+                        <img src='/images/Investment/kaagaz.png' alt=''/>
+                    </li>
+                    <li>
+                        <img src='/images/Investment/explorex.png' alt=''/>
+                    </li>
+                    <li>
+                        <img src='/images/Investment/opslyft.png' alt=''/>
+                    </li>
+                </ul> */}
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
